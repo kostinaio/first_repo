417f8701ed8565430dd53e86b29868c0be14c77b
kostinaio <kostinaio2209@gmail.com>
2024-02-22 15:13:41 +0200

    add hw1 and new oleg changes

---
+++ module01/main.py
@@ -0,0 +1,12 @@
+num = int(-16)
+
+if not num % 1:
+    if num % 2:
+        result = "Positive odd number"
+    else:
+        result = "Positive even number"
+elif num < 0:
+    result = "Negative number"
+else:
+    result = "It is zero"
+print (result)
